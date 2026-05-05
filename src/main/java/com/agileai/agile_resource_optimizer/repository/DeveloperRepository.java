91b745983aa14880dc6f89a9a562b5a62c96d77d
hirun athukorala <hirunbhathiya@gmail.com>
2026-05-05 23:02:32 +0530

    new cahnes to BE

--- src/main/java/com/agileai/agile_resource_optimizer/repository/DeveloperRepository.java
+++
@@ -1,8 +0,0 @@
-package com.agileai.agile_resource_optimizer.repository;
-
-
-
-import com.agileai.agile_resource_optimizer.entity.Developer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeveloperRepository extends JpaRepository<Developer, Long> {}
--- src/main/java/com/agileai/agile_resource_optimizer/repository/TaskRepository.java
+++
@@ -1,6 +0,0 @@
-package com.agileai.agile_resource_optimizer.repository;
-
-import com.agileai.agile_resource_optimizer.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {}
